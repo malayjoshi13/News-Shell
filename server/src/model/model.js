78526cb2ed1f6cf0c7531fb3cf68e51acd802f45
Aryan <Aryans8791@gmail.com>
2022-07-31 01:53:00 +0530

    database

---
+++ server/src/model/model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const newsSchema = new mongoose.Schema(
+    {
+        heading:{
+            type: String,
+            required: true,
+            unique: true
+        },
+        news:{
+            type: String,
+            required: true
+        },
+        newsUrl:{
+            type: String,
+            required: true
+        },
+        newsDate:{
+            type: String,
+            required: true
+        },
+        imageUrl:{
+            type: String,
+            required: true
+        },
+        source:{
+            type: String,
+            required: true
+        },
+        category:{
+            type: String,
+            required: true
+        }
+    }
+)
+const NewsModel = new mongoose.model("new", newsSchema)
+module.exports = NewsModel
