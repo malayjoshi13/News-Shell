350318384e36c2bbda17bf84cc6995570b4ea589
Aryan <Aryans8791@gmail.com>
2022-08-08 17:19:18 +0530

    python using node

--- server/src/modelLoad.js
+++ server/src/modelLoad.js
@@ -1,24 +1,24 @@
-const { exec } = require('child_process');
-const path = require("path")
-pathName=path.join(__dirname,"index.py")
+// const { exec } = require('child_process');
+// const path = require("path")
+// pathName=path.join(__dirname,"index.py")
 // console.log("ghfhf");
-const executePy = (pathName) => {
-    // console.log("vgfgf");
-    return new Promise((resolve, reject) => {
-        // console.log("ghgh");
-        exec(`python ${pathName}`, 
-            (error, stdout, stderr) => {
-                if(error){
-                    reject({error, stderr});
-                }
-                if(stderr){
-                    reject(stderr);
-                }
-                resolve(stdout);
-            });
-    });
-};
+// const executePy = (pathName) => {
+//     console.log("vgfgf");
+//         return new Promise((resolve, reject) => {
+//         console.log("ghgh");
+//         exec(`python ${pathName}`, 
+//             (error, stdout, stderr) => {
+//                 if(error){
+//                     reject({error, stderr});
+//                 }
+//                 if(stderr){
+//                     reject(stderr);
+//                 }
+//                 resolve(stdout);
+//             });
+//     });
+// };
 
-module.exports = {
-    executePy,
-}
+// module.exports = {
+//     executePy,
+// }
