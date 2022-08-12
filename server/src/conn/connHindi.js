f418da711f15f4df1adb55a4fa41d99a13d90147
AryanSinghal1 <88899023+AryanSinghal1@users.noreply.github.com>
2022-08-12 23:48:07 +0530

    Update connHindi.js

--- server/src/conn/connHindi.js
+++ server/src/conn/connHindi.js
@@ -2,9 +2,14 @@ const {MongoClient} = require('mongodb')
 require('dotenv').config()
 const client = new MongoClient(process.env.MONGODB_URL)
 
+// const dbConnect = async()=>{
+//     let result = await client.connect();
+//     db = result.db("test")
+//     return db.collection("news")
+//   }
 const dbConnect = async()=>{
     let result = await client.connect();
-    db = result.db("test")
-    return db.collection("news")
+    db = result.db("NewshellTest")
+    return db.collection("Testing")
   }
   module.exports = dbConnect;
