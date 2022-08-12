27704d8f8ed5d2fc3b00b508e83ea63d1459195b
AryanSinghal1 <88899023+AryanSinghal1@users.noreply.github.com>
2022-08-12 23:51:09 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,71 +2,83 @@ const express = require("express");
 const fs = require("fs");
 const axios = require("axios").default;
 require("./conn/conn");
-const dbConnect = require('./conn/connHindi')
+var bodyParser = require('body-parser'); 
+var request = require('request');
+const dbConnect = require("./conn/connHindi");
 const newsSchema = require("./model/model");
-const hindiNewsSchema = require("./model/hindiModel.js")
+const hindiNewsSchema = require("./model/hindiModel.js");
 const app = express();
 const port = process.env.PORT || 8000;
-const translate = require('translate')
+const translate = require("translate");
 translate.engine = "google";
-const {PythonShell} = require('python-shell')
-const path = require('path')
-let options = {
-  scriptPath: path.join(__dirname),
-  args: ['value1'],
-  mode: 'text',
-  pythonOptions: ['-u'],
-}
-PythonShell.run("index.py", options, (err, res)=>{
-  if(err) console.log(err)
-  if(res) console.log(res)
+const { PythonShell } = require("python-shell");
+const path = require("path");
+const { Console } = require("console");
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
-})
 app.all("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
-var arrayNews = []
-const main = async() =>{
+var arrayNews = [];
+var gettingNews = [];
+//Function for getting news from database
+const getNewData = async () => {
+  let sumNews = [];
   let data = await dbConnect();
-  data = await data.find().toArray()
-  try{
-  for(i=0;i<data.length;i++){
-    console.log(i, data.length)
-    let text = await translate(JSON.stringify(data[i].category), "hi");
-    let head = await translate(JSON.stringify(data[i].heading), "hi");
-    let News = await translate(JSON.stringify(data[i].news), "hi");
-    let source = await translate(JSON.stringify(data[i].source), "hi");
-    let newsDate = await translate(JSON.stringify(data[i].newsDate), "hi");
-    let obj1 = {
-      category: text.replace(/['"]+/g, ''),
-      heading: head.replace(/['"]+/g, ''),
-      news: News.replace(/['"]+/g, ''),
-      source: source.replace(/['"]+/g, ''),
-      imageUrl: data[i].imageUrl,
-      newsDate: newsDate.replace(/['"]+/g, ''),
-      newsUrl:data[i].newsUrl
+  data = await data.find().toArray();
+  data.map((e) => sumNews.push(e));
+  // console.log(sumNews)
+  console.log(sumNews.length);
+  return sumNews;
+};
+// Function for converting to Hindi
+const main = async () => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  // console.log(data)
+  try {
+    for (i = 0; i < data.length; i++) {
+      console.log(i, data.length);
+      let text = await translate(JSON.stringify(data[i].category), "hi");
+      let head = await translate(JSON.stringify(data[i].heading), "hi");
+      let News = await translate(JSON.stringify(data[i].news), "hi");
+      let source = await translate(JSON.stringify(data[i].source), "hi");
+      let newsDate = await translate(JSON.stringify(data[i].newsDate), "hi");
+      let obj1 = {
+        category: text.replace(/['"]+/g, ""),
+        heading: head.replace(/['"]+/g, ""),
+        news: News.replace(/['"]+/g, ""),
+        source: source.replace(/['"]+/g, ""),
+        imageUrl: data[i].imageUrl,
+        newsDate: newsDate.replace(/['"]+/g, ""),
+        newsUrl: data[i].newsUrl,
+      };
+      let hindiNewsData = new hindiNewsSchema({
+        heading: obj1.heading,
+        news: obj1.news,
+        newsUrl: obj1.newsUrl,
+        newsDate: obj1.newsDate,
+        imageUrl: obj1.imageUrl,
+        source: obj1.source,
+        category: obj1.category[0],
+      });
+      await hindiNewsData.save();
+      console.log(data.length);
     }
-    let hindiNewsData = new hindiNewsSchema({
-      heading: obj1.heading,
-      news: obj1.news,
-      newsUrl: obj1.newsUrl,
-      newsDate: obj1.newsDate,
-      imageUrl: obj1.imageUrl,
-      source: obj1.source,
-      category: obj1.category[0],
-    });
-          await hindiNewsData.save();
-          console.log(data.length)
-        }
-        arrayNews.push(obj1);
-        i++
-      }catch{
-        (e)=>{console.log(e)
-console.log("Hello")}}
-}
-var a = "0"
+    arrayNews.push(obj1);
+    i++;
+  } catch {
+    (e) => {
+      console.log(e);
+      console.log("Hello");
+    };
+  }
+};
+var a = "0";
+//Function for inserting News into Database
 const getPosts = async () => {
   const Topics = [
     "top",
@@ -96,31 +108,54 @@ const getPosts = async () => {
             source: newsArray[i].source_id,
             category: newsArray[i].category[0],
           });
-          try{
-          await newsData.save();
-          }catch{(e)=>{
-            console.log(e)
+          try {
+            await newsData.save();
+          } catch {
+            (e) => {
+              console.log(e);
+            };
           }
-        }
         }
       })
       .catch((e) => {
         console.log(e);
       });
     });
+  };
+  var count = "0";
+  const getCount = () => {
+  count = count + "1";
 };
-var count = "0"
-const getCount=()=>{
-  count=count+"1"
-} 
-main()
-app.get("/", (req, resp) => {
-  resp.send(arrayNews);
+main();
+//Function for sending data to Flask
+const sendIt = async(req, res)=>{
+  const getData = await getNewData()
+  // getData.map((e)=>{
+    var data = {news:getData[0].news}
+    const options={
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+    request.post('http://127.0.0.1:5000/get', options)
+  // })
+}
+sendIt()
+app.get("/", async(req, res) => {
+  //Function for getting data from Flask
+      request('http://127.0.0.1:5000/get', function (error, response, body) {
+      console.error('error:', error); // Print the error
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the data received
+      res.send(body); //Display the response on the website
+});      
 });
 setInterval(() => {
-    getPosts();
-    getCount();
-  }, 270000);
+  getPosts();
+  getCount();
+}, 270000);
 app.get("/sumarize", (req, res) => {
   data = {};
   fs.readFile("data.json", (err, fd) => {
