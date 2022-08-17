6f6a81b0c51c6acb16db91643dc7db98f5220d0e
AryanSinghal1 <88899023+AryanSinghal1@users.noreply.github.com>
2022-08-17 13:40:29 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -11,12 +11,8 @@ const app = express();
 const port = process.env.PORT || 8000;
 const translate = require("translate");
 translate.engine = "google";
-const { PythonShell } = require("python-shell");
-const path = require("path");
-const { Console } = require("console");
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
 app.all("/*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -24,7 +20,6 @@ app.all("/*", function (req, res, next) {
 });
 var arrayNews = [];
 var gettingNews = [];
-//Function for getting news from database
 const getNewData = async () => {
   let sumNews = [];
   let data = await dbConnect();
@@ -34,19 +29,20 @@ const getNewData = async () => {
   console.log(sumNews.length);
   return sumNews;
 };
-// Function for converting to Hindi
+
 const main = async () => {
   let data = await dbConnect();
   data = await data.find().toArray();
-  // console.log(data)
-  try {
+  console.log(data)
+  // try {
     for (i = 0; i < data.length; i++) {
       console.log(i, data.length);
-      let text = await translate(JSON.stringify(data[i].category), "hi");
-      let head = await translate(JSON.stringify(data[i].heading), "hi");
-      let News = await translate(JSON.stringify(data[i].news), "hi");
-      let source = await translate(JSON.stringify(data[i].source), "hi");
-      let newsDate = await translate(JSON.stringify(data[i].newsDate), "hi");
+      let text = data[i].category!=""?await translate(JSON.stringify(data[i].category), "hi"):data[i].category;
+      let head = data[i].head!=""?await translate(JSON.stringify(data[i].heading), "hi"):data[i].head;
+      let News = data[i].News!=""?await translate(JSON.stringify(data[i].news), "hi"):data[i].News;
+      let source = data[i].source!=""?await translate(JSON.stringify(data[i].source), "hi"):data[i].source;
+      let newsDate = data[i].newsDate!=""?await translate(JSON.stringify(data[i].newsDate), "hi"):data[i].newsDate;
+      console.log(text, head, News, source, newsDate);
       let obj1 = {
         category: text.replace(/['"]+/g, ""),
         heading: head.replace(/['"]+/g, ""),
@@ -56,29 +52,28 @@ const main = async () => {
         newsDate: newsDate.replace(/['"]+/g, ""),
         newsUrl: data[i].newsUrl,
       };
-      let hindiNewsData = new hindiNewsSchema({
-        heading: obj1.heading,
-        news: obj1.news,
-        newsUrl: obj1.newsUrl,
-        newsDate: obj1.newsDate,
-        imageUrl: obj1.imageUrl,
-        source: obj1.source,
-        category: obj1.category[0],
-      });
-      await hindiNewsData.save();
-      console.log(data.length);
+      // let hindiNewsData = new hindiNewsSchema({
+      //   heading: obj1.heading,
+      //   news: obj1.news,
+      //   newsUrl: obj1.newsUrl,
+      //   newsDate: obj1.newsDate,
+      //   imageUrl: obj1.imageUrl,
+      //   source: obj1.source,
+      //   category: obj1.category[0],
+      // });
+      // await hindiNewsData.save();
     }
-    arrayNews.push(obj1);
+    // arrayNews.push(obj1);
     i++;
-  } catch {
-    (e) => {
-      console.log(e);
-      console.log("Hello");
-    };
-  }
+  // } catch {
+    // (e) => {
+      // console.log(e);
+      // console.log("Hello");
+    // };
+  // }
 };
+main()
 var a = "0";
-//Function for inserting News into Database
 const getPosts = async () => {
   const Topics = [
     "top",
@@ -127,31 +122,58 @@ const getPosts = async () => {
   count = count + "1";
 };
 main();
-//Function for sending data to Flask
-const sendIt = async(req, res)=>{
-  const getData = await getNewData()
-  // getData.map((e)=>{
-    var data = {news:getData[0].news}
-    const options={
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }
-    request.post('http://127.0.0.1:5000/get', options)
-  // })
+const sleep = async(milli) =>{
+  return new Promise(resolve=>setTimeout(resolve, milli))
 }
-sendIt()
-app.get("/", async(req, res) => {
-  //Function for getting data from Flask
-      request('http://127.0.0.1:5000/get', function (error, response, body) {
-      console.error('error:', error); // Print the error
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the data received
-      res.send(body); //Display the response on the website
-});      
-});
+// const sendIt = async(req, res)=>{
+//   const getData = await getNewData()
+//   let datafrom = await dbConnect()
+//   console.log(datafrom)
+//     for(i=0;i<getData.length;i++){
+//     var data = getData[1]
+//     console.log(data)
+//     const options={
+//       method: 'POST',
+//       headers:{
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(data)
+//     }
+//     request.post('http://127.0.0.1:5000/get', options)
+//     await sleep(900000)
+//     request('http://127.0.0.1:5000/get',async function (error, response, body) {
+//       console.error('error:', error); 
+//       console.log('statusCode:', response && response.statusCode);
+//     const bodyy = JSON.parse(body)
+//       console.log(bodyy)
+//       console.log(bodyy.news)
+//       console.log(bodyy.heading)
+//       await datafrom.updateOne({heading:bodyy.heading},{
+//           $set: {
+//               news: bodyy.news,
+//               simplify: bodyy.simplify
+//             }
+          
+//         })
+//         })
+//       }
+//     }
+      // sendIt()
+      app.get("/", async(req, res) => {
+        // request('http://127.0.0.1:5000/get',async function (error, response, body) {
+          // console.error('error:', error); // Print the error
+          // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          // console.log(body);
+          // const bodyy = JSON.parse(body)
+          // console.log(bodyy.news)
+        })
+    // request('http://127.0.0.1:5000/get',async function (error, response, body) {
+    //   console.error('error:', error); // Print the error
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //   console.log(body)
+    // });
+  // })
+// });
 setInterval(() => {
   getPosts();
   getCount();
